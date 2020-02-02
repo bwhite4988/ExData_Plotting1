dce35407c0bcf9645c638bdaa55e0905bce47852
Brandon <57969524+bwhite4988@users.noreply.github.com>
2020-02-02 15:29:22 -0500

    Add files via upload

---
+++ plot 1.r
@@ -0,0 +1,6 @@
+ ## Create the histogram
+  hist(t$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
+  
+  ## Saving to file
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
